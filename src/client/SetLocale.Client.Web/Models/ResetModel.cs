9ccb4a258c291bd7986a4687295f729c0da8be2f
rmzsmr <ramiz.sumerr@gmail.com>
2013-12-23 15:18:00 +0200

    user reset

---
+++ src/client/SetLocale.Client.Web/Models/ResetModel.cs
@@ -0,0 +1,19 @@
+﻿using SetLocale.Util;
+
+namespace SetLocale.Client.Web.Models
+{
+    public class ResetModel : BaseModel
+    {
+        public string Email { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(Email)
+                   && Email.IsEmail();
+        }
+
+
+    }
+
+   
+}
