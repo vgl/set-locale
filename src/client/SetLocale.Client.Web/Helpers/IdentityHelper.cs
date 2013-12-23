97d444d4a75f4ff281dde0e04fe56bc640512ed1
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-23 17:16:24 +0200

    menu added

---
+++ src/client/SetLocale.Client.Web/Helpers/IdentityHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace SetLocale.Client.Web.Helpers
+{
+    public static class IdentityHelper
+    {
+        public static int GetUserId(this IIdentity identity)
+        {
+            return identity.IsAuthenticated ? Convert.ToInt32(identity.Name.Split('|')[0]) : 0;
+        }
+
+        public static string GetUserFullName(this IIdentity identity)
+        {
+            return identity.IsAuthenticated ? identity.Name.Split('|')[1] : string.Empty;
+        }
+    }
+}
