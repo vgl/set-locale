879e10ccb6fd91e4390de0033bdd262fd1ef427a
Cihan Coşkun <cihancoskun@gmail.com>
2013-12-24 15:42:09 +0200

    *New Page - Key Listing *KeyModel modified *LanguageModel for KeyModel.Languages *Modified - View/Key Index Page

--- src/client/SetLocale.Client.Web/Controllers/BaseController.cs
+++ src/client/SetLocale.Client.Web/Controllers/BaseController.cs
@@ -14,6 +14,9 @@ namespace SetLocale.Client.Web.Controllers
         {
             //temp usage...
             var dictionary = new Dictionary<string, string>();
+            dictionary.Add("Edit", "Edit");
+            dictionary.Add("Delete", "Delete");
+
             dictionary.Add("Login", "Login");
             dictionary.Add("Email", "Email"); 
             dictionary.Add("Password", "Password");
@@ -40,14 +43,17 @@ namespace SetLocale.Client.Web.Controllers
             dictionary.Add("Description", "Description");
 
             dictionary.Add("NewKey", "NewKey");
+            dictionary.Add("KeyListing", "KeyListing");
             dictionary.Add("Key", "Key");
             dictionary.Add("Tag", "Tag");
+            dictionary.Add("Translated", "Translated");
+            dictionary.Add("NotTranslated", "NotTranslated");
+            dictionary.Add("TranslatedLang", "TranslatedLang");
 
             dictionary.Add("Apps", "Applications");
             dictionary.Add("App", "Application");
             dictionary.Add("Token", "Token");
             dictionary.Add("CreationDate", "Creation Date");
-            dictionary.Add("Delete", "Delete");
             dictionary.Add("CreateNewToken", "Create New Token"); 
 
 
---
+++ src/client/SetLocale.Client.Web/Models/LanguageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetLocale.Client.Web.Models
+{
+    public class LanguageModel : BaseModel 
+    {
+
+        public string Language { get; set; }
+        public string Value { get; set; }
+        public string FlagImageUrl { get; set; }
+
+    }
+}
