d0c6742edb78597ee930d9f429b397e113095422
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-24 17:23:46 +0200

    test project added

---
+++ src/test/SetLocale.Client.Test/RouteTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using NUnit.Framework;
+using SetLocale.Client.Web.Configurations;
+using SetLocale.Client.Web.Controllers;
+
+namespace SetLocale.Client.Test
+{
+    [TestFixture]
+    public class RouteTests
+    {
+       
+    }
+}
