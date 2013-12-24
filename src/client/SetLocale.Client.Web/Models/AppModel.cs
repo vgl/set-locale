4eca11b1670dc9fa4eded4eb02b17cd20cb0e886
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-24 09:15:17 +0200

    admin apps listing work

---
+++ src/client/SetLocale.Client.Web/Models/AppModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SetLocale.Client.Web.Models
+{
+    public class AppModel
+    {
+        public int Id { get; set; }
+        public string UserEmail { get; set; }
+        public string AppName { get; set; }
+        public string AppDescription { get; set; }
+        public string Url { get; set; }
+        public int UsageCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
