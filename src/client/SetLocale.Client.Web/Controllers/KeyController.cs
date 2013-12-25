c9574e875b243b089650e767132092943a788100
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-25 15:12:43 +0200

    key views added

--- src/client/SetLocale.Client.Web/Controllers/KeyController.cs
+++ src/client/SetLocale.Client.Web/Controllers/KeyController.cs
@@ -72,6 +72,170 @@ namespace SetLocale.Client.Web.Controllers
         }
 
         [HttpGet]
+        public ActionResult My()
+        {
+
+            var langs = new List<LanguageModel>();
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Kayıt Ol",
+                Language = "tr",
+                FlagImageUrl = "/lang/tr"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Sign Up",
+                Language = "en",
+                FlagImageUrl = "/lang/en"
+            });
+
+            var model = new List<KeyModel>();
+            model.Add(new KeyModel
+            {
+                Key = "sign_up",
+                Description = "Kullanıcı üyelik açması için kullanılır.",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+
+            langs.Clear();
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Giriş",
+                Language = "tr",
+                FlagImageUrl = "/Public/img/tr.png"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Sign In",
+                Language = "en",
+                FlagImageUrl = "/Public/img/en.png"
+            });
+
+            model.Add(new KeyModel
+            {
+                Key = "sign_in",
+                Description = "Kullanıcı girişi için kullanılır.",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+
+            var langs = new List<LanguageModel>();
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Kayıt Ol",
+                Language = "tr",
+                FlagImageUrl = "/lang/tr"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Sign Up",
+                Language = "en",
+                FlagImageUrl = "/lang/en"
+            });
+
+            var model = new List<KeyModel>();
+            model.Add(new KeyModel
+            {
+                Key = "sign_up",
+                Description = "Kullanıcı üyelik açması için kullanılır.",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+
+            langs.Clear();
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Giriş",
+                Language = "tr",
+                FlagImageUrl = "/Public/img/tr.png"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Sign In",
+                Language = "en",
+                FlagImageUrl = "/Public/img/en.png"
+            });
+
+            model.Add(new KeyModel
+            {
+                Key = "sign_in",
+                Description = "Kullanıcı girişi için kullanılır.",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NotTranslated()
+        {
+
+            var langs = new List<LanguageModel>();
+
+            langs.Add(new LanguageModel
+            {
+                Language = "tr",
+                FlagImageUrl = "/lang/tr"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Language = "en",
+                FlagImageUrl = "/lang/en"
+            });
+
+            var model = new List<KeyModel>();
+            model.Add(new KeyModel
+            {
+                Key = "save",
+                Description = "kaydet butonu için",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+            return View(model);
+        }
+
+        [HttpGet]
         public ActionResult New()
         {
             string[] tags = { "genel" };
--- src/client/SetLocale.Client.Web/Controllers/TagController.cs
+++ src/client/SetLocale.Client.Web/Controllers/TagController.cs
@@ -1,14 +1,45 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using Castle.Core.Resource;
+using SetLocale.Client.Web.Models;
 
 namespace SetLocale.Client.Web.Controllers
 {
     public class TagController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string id)
         {
-            return View();
+            var langs = new List<LanguageModel>();
+            langs.Add(new LanguageModel
+            {
+                Value = "Kayıt Ol",
+                Language = "tr",
+                FlagImageUrl = "/lang/tr"
+            });
+
+            langs.Add(new LanguageModel
+            {
+                Value = "Sign Up",
+                Language = "en",
+                FlagImageUrl = "/lang/en"
+            });
+
+            var model = new List<KeyModel>();
+            model.Add(new KeyModel
+            {
+                Key = "sign_up",
+                Description = "Kullanıcı üyelik açması için kullanılır.",
+                IsTranslated = true,
+                Tag = new List<TagModel>()
+                {
+                    new TagModel{ Name = "Membership", UrlName = "membership"}
+                },
+                Languages = langs
+            });
+
+            return View(model);
         }
     }
 }
