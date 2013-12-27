ea5ac8b2861421709b9f3347a8b4c84c386274e8
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-27 11:29:50 +0200

    translator model removed

--- src/client/SetLocale.Client.Web/Controllers/AdminController.cs
+++ src/client/SetLocale.Client.Web/Controllers/AdminController.cs
@@ -20,19 +20,16 @@ namespace SetLocale.Client.Web.Controllers
         [HttpGet]
         public ActionResult NewTranslator()
         {
-            var model = new TranslatorModel()
-            {
-                Email = "user@test.com",
-                Name = "Translator"
-            };
+            var model = _demoDataService.GetAUser();
             return View(model);
         }
 
-        [HttpPost]
-        public ActionResult NewTranslator(TranslatorModel model)
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult NewTranslator(UserModel model)
         {
-            if (model.IsValid())
+            if (model.IsValidForAddingNewTranslator())
             {
+                //todo: create new user with translator role...
                 return Redirect("/admin/users");
             }
 
--- src/client/SetLocale.Client.Web/Models/UserModel.cs
+++ src/client/SetLocale.Client.Web/Models/UserModel.cs
@@ -1,13 +1,21 @@
-﻿namespace SetLocale.Client.Web.Models
+﻿using SetLocale.Util;
+
+namespace SetLocale.Client.Web.Models
 {
-    public class UserModel
+    public class UserModel: BaseModel
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
         public bool IsActive { get; set; }
-
         public string Language { get; set; }
+
+        public bool IsValidForAddingNewTranslator()
+        {
+            return !string.IsNullOrEmpty(Name)
+                   && !string.IsNullOrEmpty(Email)
+                   && Email.IsEmail();
+        }
     }
 }
