553d98cdb77d6cb89a823f01ca1ae8d665f463ca
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-12-30 16:02:11 +0200

    langcontroller change action should add lang cookie

--- src/test/SetLocale.Client.Web.Test/Controllers/LangControllerTests.cs
+++ src/test/SetLocale.Client.Web.Test/Controllers/LangControllerTests.cs
@@ -1,11 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 
 using Moq;
 using NUnit.Framework;
-using Rhino.Mocks;
 
 using SetLocale.Client.Web.Controllers;
-using SetLocale.Client.Web.Models;
 using SetLocale.Client.Web.Services;
 
 namespace SetLocale.Client.Web.Test.Controllers
@@ -14,21 +13,30 @@ namespace SetLocale.Client.Web.Test.Controllers
     public class LangControllerTests
     {
         [Test]
-        public void change_should_return_add_lang_cookie()
+        public void change_should_add_lang_cookie()
         {
             // Arrange
+            var controllerContext = new Mock<ControllerContext>();
+            var httpContext = new Mock<HttpContextBase>();
+            controllerContext.Setup(x => x.HttpContext).Returns(httpContext.Object);
+
+            var httpRequest = new Mock<HttpRequestBase>();
+            var httpResponse = new Mock<HttpResponseBase>();
+            httpContext.Setup(x => x.Request).Returns(httpRequest.Object);
+            httpContext.Setup(x => x.Response).Returns(httpResponse.Object);
+            httpResponse.Setup(x => x.SetCookie(It.IsAny<HttpCookie>()));
+            
             var authService = new Mock<IFormsAuthenticationService>();
             var demoService = new Mock<IDemoDataService>();
             
-            //todo: mock https response
-
             // Act
             var controller = new LangController(authService.Object, demoService.Object);
+            controller.ControllerContext = controllerContext.Object;
+            
             var view = controller.Change("tr");
 
             // Assert
             Assert.NotNull(view);
         }
-         
     }
 }
