d867eff991d6d56d095304c927fa1a66b21bac33
Mehmet Sabancıoğlu <mehmet.sabancioglu@gmail.com>
2013-12-31 09:53:41 +0200

    Admin Controller Test

--- src/test/SetLocale.Client.Web.Test/Controllers/AdminControllerTests.cs
+++ src/test/SetLocale.Client.Web.Test/Controllers/AdminControllerTests.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Moq;
 using NUnit.Framework;
+
 using SetLocale.Client.Web.Controllers;
 using SetLocale.Client.Web.Services;
+using SetLocale.Client.Web.Test.TestHelpers;
+using System.Web.Mvc;
+using SetLocale.Client.Web.Models;
 
 namespace SetLocale.Client.Web.Test.Controllers
 {
@@ -14,28 +19,100 @@ namespace SetLocale.Client.Web.Test.Controllers
     class AdminControllerTests
     {
         [Test]
-        public void admin_index_test()
+        public void index_should_return_user_model()
         {
-            // Arrange
-            var authService = new Mock<IFormsAuthenticationService>();
-            var demoService = new Mock<IDemoDataService>();
-
-            //todo: mock https response
-
             // Act
-            var controller = new AdminController(authService.Object, demoService.Object);
-            var view = controller.NewTranslator();
-            var view2 = controller.Users();
-            var view3 = controller.Apps();
-
-            demoService.Verify(x => x.GetAllApps(), Times.Once);
-            demoService.Verify(x => x.GetAllUsers(), Times.Once);
-            demoService.Verify(x => x.GetAUser(), Times.Once);
+            var controller = new AdminController(null, null);
+            var view = controller.Index();
 
             // Assert
             Assert.NotNull(view);
-            Assert.NotNull(view2);
-            Assert.NotNull(view3);
+            controller.AssertGetAttribute("Index");
+            
         }
+
+             [Test]
+             public void new_translator_should_return_user_model()
+             {
+                 // Arrange           
+                 var demoService = new Mock<IDemoDataService>();
+
+                 // Act
+                 var controller = new AdminController(null, demoService.Object);
+                 var view = controller.NewTranslator();
+
+                 // Assert
+                 Assert.NotNull(view);
+                 controller.AssertGetAttribute("NewTranslator");
+                 demoService.Verify(x => x.GetAUser(), Times.Once);
+             }
+
+             [Test]
+             public void new_translator_should_redirect_if_model_is_valid()
+             {
+                 // Arrange
+                 var validModel = new UserModel { Name = "test name", Email = "test@test.com" };
+
+                 // Act
+                 var controller = new AdminController(null, null);
+                 var view = controller.NewTranslator(validModel) as RedirectResult;
+
+                 // Assert
+                 Assert.NotNull(view);
+                 Assert.AreEqual(view.Url, "/admin/users");
+                 controller.AssertPostAttribute("NewTranslator", new[] { typeof(UserModel) });
+             }
+
+             [Test]
+             public void new_translator_should_return_app_model_if_model_is_invalid()
+             {
+                 // Arrange
+                 var inValidModel = new UserModel { Name = "test name" };
+
+                 // Act
+                 var controller = new AdminController(null, null);
+                 var view = controller.NewTranslator(inValidModel) as ViewResult;
+
+                 // Assert
+                 Assert.NotNull(view);
+                 Assert.NotNull(view.Model);
+                 var model = view.Model as UserModel;
+
+                 Assert.NotNull(model);
+
+                 controller.AssertPostAttribute("NewTranslator", new[] { typeof(UserModel) });
+             }
+
+             [Test]
+             public void Users_should_return_app_model()
+             {
+                 // Arrange           
+                 var demoService = new Mock<IDemoDataService>();
+
+                 // Act
+                 var controller = new AdminController(null, demoService.Object);
+                 var view = controller.Users();
+
+                 // Assert
+                 Assert.NotNull(view);
+                 controller.AssertGetAttribute("Users");
+                 demoService.Verify(x => x.GetAllUsers(), Times.Once);
+             }
+
+             [Test]
+             public void apps_should_return_app_model()
+             {
+                 // Arrange           
+                 var demoService = new Mock<IDemoDataService>();
+
+                 // Act
+                 var controller = new AdminController(null, demoService.Object);
+                 var view = controller.Apps();
+
+                 // Assert
+                 Assert.NotNull(view);
+                 controller.AssertGetAttribute("Apps");
+                 demoService.Verify(x => x.GetAllApps(), Times.Once);
+             }       
     }
 }
