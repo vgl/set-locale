bbdb1c62dd7bf26db40b1750b337ae07267ea44e
Cihan Coşkun <cihancoskun@gmail.com>
2014-01-02 16:25:13 +0200

    new 18 Words and 4 Apps

--- src/client/SetLocale.Client.Web/Repositories/SetLocaleDbInitializer.cs
+++ src/client/SetLocale.Client.Web/Repositories/SetLocaleDbInitializer.cs
@@ -20,14 +20,33 @@ namespace SetLocale.Client.Web.Repositories
 
             AddTranslator(context, "Kemal Çolak", "kml.colak@gmail.com");
             AddTranslator(context, "deneme kullanıcı", "deneme@gmail.com");
-
-            AddApplication(context, "setlocale@test.com", "SetLocale", "a localization management application.","setlocale.com");
+             
+            AddApplication(context, "setlocale@test.com", "SetLocale", "a localization management application.", "setlocale.com");
             AddApplication(context, "setcrm@test.com", "SetCrm", "a brand new crm application.", "setcrm.com");
+            AddApplication(context, "drone@test.com", "Marmara Drone", "a wireless control dashboard for humanless flying planes.", "github.com/jupre/marmaradrone");
+            AddApplication(context, "collade@test.com", "Collade", "a task management and team collaboration application.", "marmaradrone.github.io");
+           
+            AddWord(context, "app_name", "Uyguluma ismi", "Set Locale", "Set Locale", "SetLocale", "set-locale");
+            AddWord(context, "menu_words", "Kelimeler Menüsü için.", "Kelimeler", "Words", "SetLocale", "set-locale");
+            AddWord(context, "menu_words_words", "Kelimeler Menüsü için.", "Tüm Kelimeler", "All Words", "SetLocale", "set-locale");
+            AddWord(context, "menu_words_my_words", "Kelimeler Menüsü için.", "Kelimelerim", "My Words", "SetLocale", "set-locale");
+            AddWord(context, "menu_words_new_word", "Kelimeler Menüsü için.", "Yeni Kelime", "New Word", "SetLocale", "set-locale");
+            AddWord(context, "menu_words_not_translated", "Kelimeler Menüsü için.", "Çevrilmeyenler", "Not Translated", "SetLocale", "set-locale");
 
-            AddWord(context, "btn_save", "kaydet butonu için", "tr", "en", "SetMembership", "set-membership");
-            AddWord(context, "btn_save", "kaydet butonu için", "tr", "en", "SetMembership", "set-membership");
-            AddWord(context, "btn_save", "kaydet butonu için", "tr", "en", "SetLocale", "set-locale");
-            AddWord(context, "btn_save", "kaydet butonu için", "tr", "en", "SetLocale", "set-locale");
+            AddWord(context, "menu_apps", "Uygulamalar Menüsü için.", "Uygulamalar", "Applications", "SetLocale", "set-locale");
+            AddWord(context, "menu_apps_apps", "Uygulamalar Menüsü için.", "Uygulamalarım", "My Applications", "SetLocale", "set-locale");
+            AddWord(context, "menu_apps_new_app", "Uygulamalar Menüsü için.", "Yeni Uygulama", "New Application", "SetLocale", "set-locale");
+
+            AddWord(context, "menu_settings", "Admin İşlemleri Menüsü İçin.", "Admin İşlemleri", "Administrator", "SetLocale", "set-locale");
+            AddWord(context, "menu_settings_apps", "Admin İşlemleri Menüsü İçin.", "Uygulamalar", "All Applications", "SetLocale", "set-locale");
+            AddWord(context, "menu_settings_users", "Admin İşlemleri Menüsü İçin.", "Kullanıcılar", "All Users", "SetLocale", "set-locale");
+            AddWord(context, "menu_settings_new_translator", "Admin İşlemleri Menüsü İçin.", "Yeni Çevirmen", "New Translator", "SetLocale", "set-locale");
+
+            AddWord(context, "menu_user_login", "Kullanıcı Giriş Menüsü", "Giriş", "Login", "SetLocale", "set-locale");
+            AddWord(context, "menu_user_logout", "Kullanıcı Giriş Menüsü", "Çıkış", "Logout", "SetLocale", "set-locale");
+            AddWord(context, "menu_user_sign_up", "Kullanıcı Giriş Menüsü", "Kayıt Ol", "Signup", "SetLocale", "set-locale");
+            AddWord(context, "menu_user_reset", "Kullanıcı Giriş Menüsü", "Şifre Sıfırla", "Reset Password", "SetLocale", "set-locale");
+             
 
             context.SaveChanges();
         }
