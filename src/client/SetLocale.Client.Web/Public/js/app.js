d290be46eab7a6de1bc643d1cf4c85302998f502
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-03 11:02:08 +0200

    user apps deactivate activate work

--- src/client/SetLocale.Client.Web/Public/js/app.js
+++ src/client/SetLocale.Client.Web/Public/js/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(function() {
+    $("a.btnAction").click(function() {
+        var textBtn = "input#btnModalAction";
+        var textBtnDanger = "btn-danger";
+        var textBtnSuccess = "btn-success";
+        var textId = "id";
+        var textIsActive = "isactive";
+
+        var id = $(this).data(textId);
+        var isActive = $(this).data(textIsActive);
+        
+        $(textBtn).removeClass(textBtnDanger).removeClass(textBtnSuccess);
+        console.log($(textBtn));
+        if (isActive == "True") {
+            $(textBtn).addClass(textBtnDanger);
+        } else {
+            $(textBtn).addClass(textBtnSuccess);
+        }
+
+        $(textBtn).data(textId, id).data(textIsActive, isActive);
+    });
+});
