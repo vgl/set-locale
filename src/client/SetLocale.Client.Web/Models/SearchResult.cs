4ef41fe922ce7f6b5052d262778799f8b634077a
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-06 14:48:48 +0200

    search service

---
+++ src/client/SetLocale.Client.Web/Models/SearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SetLocale.Client.Web.Models
+{
+    public class SearchResult
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
