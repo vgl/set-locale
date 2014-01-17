48bcacf129a9207aba1ba5d16afe8cd4be4eef2d
Cihan Coşkun <cihancoskun@gmail.com>
2014-01-17 17:37:20 +0200

    some changes

--- src/client/SetLocale.Client.Web/Global.asax.cs
+++ src/client/SetLocale.Client.Web/Global.asax.cs
@@ -94,7 +94,7 @@ namespace SetLocale.Client.Web
             enTexts.Add("btn_cancel", "Cancel");
             enTexts.Add("btn_ok", "Ok");
 
-            enTexts.Add("home_summary", "SetLocale's <strong>{2}</strong> translator provided <strong>{4}</strong> translation for <strong><a href='/word/all' style='text-decoration:underline;color:red;'>{3}</a></strong> keys and <strong>{0}</strong> developer is consuming this service with <strong>{1}</strong> application");
+            enTexts.Add("home_summary", "SetLocale's <strong>{2}</strong> translator provided <strong>{4}</strong> translation for <strong><a href='/word/all' id='aAllWords' style='text-decoration:underline;color:red;'>{3}</a></strong> keys and <strong>{0}</strong> developer is consuming this service with <strong>{1}</strong> application");
 
             enTexts.Add("home_title", "Localization as a service");
             enTexts.Add("words_key_listing_title", "Key Listing");
@@ -179,7 +179,7 @@ namespace SetLocale.Client.Web
             trTexts.Add("btn_cancel", "İptal");
             trTexts.Add("btn_ok", "Tamam");
 
-            trTexts.Add("home_summary", "SetLocale uygulamasında <strong>{0}</strong> geliştirici tarafından eklenen <strong>{1}</strong> uygulamaya <strong>{2}</strong> çevirmen ile <strong>{3}</strong> farklı kelime için <strong>{4}</strong> adet çeviri eklenmiştir.");
+            trTexts.Add("home_summary", "SetLocale uygulamasında <strong>{0}</strong> geliştirici tarafından eklenen <strong>{1}</strong> uygulamaya <strong>{2}</strong> çevirmen ile <strong><a href='/word/all' id='aAllWords' style='text-decoration:underline;color:red;'>{3}</a></strong> farklı kelime için <strong>{4}</strong> adet çeviri eklenmiştir.");
 
             trTexts.Add("home_title", "Set-locale Servisine Hoş Geldiniz");
             trTexts.Add("words_key_listing_title", "Anahtar Listesi");
