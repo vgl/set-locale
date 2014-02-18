b27a8eb87968969f5cfc3f9d27f94a21bb1f73ac
Berat Bilgin <bbilginn@gmail.com>
2014-02-18 17:53:07 +0200

    Search result set to words collection.

--- sources/set.locale/Data/Services/SearchService.cs
+++ sources/set.locale/Data/Services/SearchService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using set.locale.Helpers;
 using set.locale.Models;
 
 namespace set.locale.Data.Services
@@ -23,11 +23,25 @@ namespace set.locale.Data.Services
                 keys = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             }
 
-            var items = Context.DomainObjects.Where(x => !x.IsDeleted);
+            var items = Context.Words.Where(x => !x.IsDeleted);
             foreach (var key in keys)
             {
                 var _key = key;
-                items = items.Where(x => x.Name.Contains(_key));
+                items = items.Where(x => x.Name.Contains(_key)
+                                      || x.Key.Contains(_key)
+                                      || x.Description.Contains(_key)
+                                      || x.Translation_AZ.Contains(_key)
+                                      || x.Translation_CN.Contains(_key)
+                                      || x.Translation_EN.Contains(_key)
+                                      || x.Translation_FR.Contains(_key)
+                                      || x.Translation_GR.Contains(_key)
+                                      || x.Translation_IT.Contains(_key)
+                                      || x.Translation_KZ.Contains(_key)
+                                      || x.Translation_RU.Contains(_key)
+                                      || x.Translation_SP.Contains(_key)
+                                      || x.Translation_TK.Contains(_key)
+                                      || x.Translation_TR.Contains(_key)
+                     );
             }
 
             var list = items.OrderByDescending(x => x.CreatedAt).Skip(0).Take(10).ToList();
@@ -35,8 +49,8 @@ namespace set.locale.Data.Services
             {
                 result.Add(new SearchResult
                 {
-                    Name = item.Name,
-                    Url = string.Format("/domainobject/detail/{0}", item.Id),
+                    Name = string.Format("{0}, {1}", item.Key, item.Key.Localize()),
+                    Url = string.Format("/word/detail/{0}", item.Id),
                     ImgUrl = string.Empty
                 });
             }
